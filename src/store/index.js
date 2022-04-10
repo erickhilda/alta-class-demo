3e103c57d30af0cc8b6d0fde3fac9ddb711a7c63
Erick Hilda <erick.hilda@ajaib.co.id>
2022-04-11 01:29:47 +0700

    chore: use environment variable to store api key

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
 import { createStore } from "vuex";
 
+const API_KEY = import.meta.env.VITE_APP_NEWS_API_KEY;
+
 export const store = createStore({
   state() {
     return {
@@ -26,7 +28,6 @@ export const store = createStore({
       commit("increment");
     },
     fetchArticles({ commit }, { query, category, countryId }) {
-      const API_KEY = "2855be0bd75a436cb0f78dd5e74313cf";
       const API_URL = `https://newsapi.org/v2/top-headlines?country=${countryId}&category=${category}&q=${query}&apiKey=${API_KEY}`;
       commit("setLoading", true);
       fetch(API_URL)
